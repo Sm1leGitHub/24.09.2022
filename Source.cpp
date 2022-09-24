86b1d7ba990d3b4dbe9881f22ba46685dffcc927
Sm1leGitHub <113791071+Sm1leGitHub@users.noreply.github.com>
2022-09-24 19:21:26 +0300

    Add files via upload

---
+++ Source.cpp
@@ -0,0 +1,99 @@
+﻿#include <iostream>;
+using namespace std;
+int main() {
+	//Boolean1◦.Дано целое число A.Проверить истинность высказывания : «Число A является положительным»
+	/*int a; 
+	bool res;
+	cin >> a;
+	res = a > 0;
+	cout << boolalpha << res << endl;*/
+	//Boolean2◦.Дано целое число A.Проверить истинность высказывания : «Число A является нечетным».
+	/*int a;
+	bool res;
+	cin >> a;
+	res = a % 2 !=0;
+	cout << boolalpha << res << endl;*/
+	//Boolean3◦.Дано целое число A.Проверить истинность высказывания : «Число A является четным».
+	/*int a;
+	bool res;
+	cin >> a;
+	res = a % 2 == 0;
+	cout << boolalpha << res << endl;*/
+	/*int a,b;
+	bool res;
+	cin >> a;
+	cin >> b;
+	res = a > 2 && b <= 3;
+	cout << boolalpha << res << endl;*/
+	//Boolean5◦.Даны два целых числа : A, B.Проверить истинность высказывания :«Справедливы неравенства A ≥ 0 или B < −2».
+	/*int a, b;
+	bool res; 
+	cin >> a;
+	cin >> b;
+	res = a >= 0 || b < -2;
+	cout << boolalpha << res << endl;*/
+	//Boolean6◦	.Даны три целых числа : A, B, C.Проверить истинность высказывания : «Справедливо двойное неравенство A < B < C»
+	/*int a, b,c;
+	bool res;
+	cin >> a;
+	cin >> b;
+	cin >> c;
+	res = a < b && b < c ;
+	cout << boolalpha << res << endl;*/
+	//Boolean7◦.Даны три целых числа : A, B, C.Проверить истинность высказывания : «Число B находится между числами A и C».
+	/*int a, b, c;
+	bool res;
+	cin >> a;
+	cin >> b;
+	cin >> c;
+	res = a < b && b < c || c < b && b < a;
+	cout << boolalpha << res << endl;*/
+	//Boolean8◦.Даны два целых числа : A, B.Проверить истинность высказывания «Каждое из чисел A и B нечетное».
+	/*int a,b;
+	bool res;
+	cin >> a;
+	cin >> b;
+	res = a % 2 != 0 && b % 2 != 0;
+	cout << boolalpha << res << endl;*/
+	//Boolean9◦.Даны два целых числа : A, B.Проверить истинность высказывания :«Хотя бы одно из чисел A и B нечетное».
+	/*int a, b;
+	bool res;
+	cin >> a;
+	cin >> b;
+	res = a % 2 != 0 && b % 2 == 0;
+	cout << boolalpha << res << endl;*/
+	//Boolean10◦.Даны два целых числа : A, B.Проверить истинность высказывания : «Ровно одно из чисел A и B нечетное».
+	/*int a, b;
+	bool res;
+	cin >> a;
+	cin >> b;
+	res = (a+b)%2 | = 0;
+	cout << boolalpha << res << endl;*/
+	//Boolean11◦	.Даны два целых числа : A, B.Проверить истинность высказывания : «Числа A и B имеют одинаковую четность».
+	/*int a, b;
+	bool res,res1;
+	cin >> a;
+	cin >> b;
+	res = a < b;
+	res1 = b < a;
+	cout << boolalpha << res << endl;
+	cout << boolalpha << res1 << endl;*/
+	//Boolean12◦.Даны три целых числа : A, B, C.Проверить истинность высказывания : «Каждое из чисел A, B, C положительное».
+	/*int a, b,c;
+	bool res, res1;
+	cin >> a;
+	cin >> b;
+	cin >> c;
+	res = a % b !=0;
+	res1 = b % a !=0;
+	cout << boolalpha << res << endl;
+	cout << boolalpha << res1 << endl;*/
+
+
+
+
+
+
+
+
+}
